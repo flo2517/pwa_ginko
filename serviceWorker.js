04b0dfe95b1b669e5f193543497558f5db899211
Florian <florianjeandenans@gmail.com>
2022-11-28 17:21:09 +0100

    add worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,35 +1,49 @@
-var cacheName = 'pwa-task-manager';
-var filesToCache = [
-    '/index.html',
-    '/style.css',
-    '/index.js',
-    '/app.js',
-    '/ginkobus.webmanifest',
-    'icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
-    '/icons/maskable_icon.png'
+const cacheName = 'cache';
+const appShellFiles = [ 
+            "icons/favicon.ico",
+            "icons/icon-32.png",
+            "icons/icon-64.png",
+            "icons/icon-96.png",
+            "icons/icon-128.png",
+            "icons/icon-168.png",
+            "icons/icon-180.png",
+            "icons/icon-192.png",
+            "icons/icon-256.png",
+            "icons/icon-512.png",
+            "icons/maskable_icon.png",
+            "app.js",
+            "index.html",
+            "style.css"
 ];
+
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(filesToCache);
-        })
-    );
+    e.waitUntil(async function() {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        //check if all ressources give 200
+        while (appShellFiles.length) {
+            const url = appShellFiles.pop();
+            const response = await fetch(url);
+            if (response.status !== 200) {
+                throw new Error(`Request for ${url} failed with status ${response.status}`);
+            }
+            await cache.put(url, response);
+        }
+    }());
 });
 
-self.addEventListener('activate', (e) => {
-    console.log('[Service Worker] Activate');
-});
 
 self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetch', e.request.url);
-});
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
