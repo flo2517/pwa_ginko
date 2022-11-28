528a8488385ef301e97fd29c2a5ea5a5b227a46a
Florian <florianjeandenans@gmail.com>
2022-11-28 16:13:01 +0100

    add manifest

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ let URL = "https://ginkobus-server.onrender.com";
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js');
+    }
+
     
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
